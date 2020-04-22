2de51b322e44952100c80296b49445f0f2ba675e
piepie62 <christopherfeger@yahoo.com>
2020-04-22 15:14:41 -0400

    Fix SHA256 to not use nonstandard typedefs

--- source/utils/sha256.c
+++ source/utils/sha256.c
@@ -29,7 +29,7 @@
 #define SIG1(x) (ROTRIGHT(x, 17) ^ ROTRIGHT(x, 19) ^ ((x) >> 10))
 
 /**************************** VARIABLES *****************************/
-static const WORD k[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
+static const uint32_t k[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
     0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa,
     0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138,
     0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624,
@@ -37,9 +37,9 @@ static const WORD k[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x395
     0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
 
 /*********************** FUNCTION DEFINITIONS ***********************/
-void sha256_transform(SHA256_CTX* ctx, const BYTE data[])
+void sha256_transform(SHA256_CTX* ctx, const uint8_t data[])
 {
-    WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+    uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
 
     for (i = 0, j = 0; i < 16; ++i, j += 4)
         m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
@@ -93,9 +93,9 @@ void sha256_init(SHA256_CTX* ctx)
     ctx->state[7] = 0x5be0cd19;
 }
 
-void sha256_update(SHA256_CTX* ctx, const BYTE data[], size_t len)
+void sha256_update(SHA256_CTX* ctx, const uint8_t data[], size_t len)
 {
-    WORD i;
+    uint32_t i;
 
     for (i = 0; i < len; ++i)
     {
@@ -110,9 +110,9 @@ void sha256_update(SHA256_CTX* ctx, const BYTE data[], size_t len)
     }
 }
 
-void sha256_final(SHA256_CTX* ctx, BYTE hash[])
+void sha256_final(SHA256_CTX* ctx, uint8_t hash[])
 {
-    WORD i;
+    uint32_t i;
 
     i = ctx->datalen;
 
@@ -159,7 +159,7 @@ void sha256_final(SHA256_CTX* ctx, BYTE hash[])
     }
 }
 
-void sha256(unsigned char hash[], unsigned char data[], size_t len)
+void sha256(uint8_t hash[], uint8_t data[], size_t len)
 {
     SHA256_CTX ctx;
     sha256_init(&ctx);
